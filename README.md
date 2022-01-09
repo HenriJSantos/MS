5eb78d96c922da685fd53c5ecdae706bd7e76c8a
nrm <gustavonrm@gmail.com>
2022-01-09 18:47:18 +0000

    added Sumo network files + parsed aggregated file to OD Matrix

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# MS
-Repository for the MS curricular unit
+# MS - 2021/2022 - Group 6
+
+## Directory Structure
+
+```
+.
+├── code
+│   └── aggregator.py 
+├── data  
+│   └── aggregated           
+│         └── 2015-01-01.csv
+├── Sumo
+│    ├── 
+│    └── aggregated   
+├── LICENSE
+├── sensorns_location.csv
+├── significado campos.txt
+└── README.md                      
+```
+
+## How to run the project
+...
+
+121750,AEDL - A43 3+100 CT3692,"41,15524","-8,556818",Areosa - Valbom,188 
+121751,AEDL - A43 4+750 CT3693,"41,15524","-8,556818",Areosa - Valbom,188 
+duplicado
--- code/aggregator.py
+++ code/aggregator.py
@@ -2,13 +2,15 @@ import pandas as pd
 
 df = pd.read_csv("data/1P2015AEDL.csv", header=0, sep=",", low_memory=False)
 df['AGG_PERIOD_START'] = pd.to_datetime(df['AGG_PERIOD_START'])
-df = df[(df['AGG_PERIOD_START'] >= pd.to_datetime("2015-01-01")) & (df['AGG_PERIOD_START'] < pd.to_datetime("2015-01-02"))] # Get one day of data
-df = df.sort_values(by=['EQUIPMENTID','AGG_PERIOD_START'])
+df = df[(df['AGG_PERIOD_START'] >= pd.to_datetime("2015-01-01")) &
+        (df['AGG_PERIOD_START'] < pd.to_datetime("2015-01-02"))]  # Get one day of data
+df = df.sort_values(by=['EQUIPMENTID', 'AGG_PERIOD_START'])
 df = df.drop(columns=[
-    'AGGREGATE_BY_LANE_BUNDLEID','AGG_ID','AGG_PERIOD_LEN_MINS','NR_LANES',
-    'LIGHT_VEHICLE_RATE','VOLUME_CLASSE_A','VOLUME_CLASSE_B','VOLUME_CLASSE_C',
-    'VOLUME_CLASSE_D','VOLUME_CLASSE_0','AVG_SPEED_ARITHMETIC','AVG_SPEED_HARMONIC',
-    'AXLE_CLASS_VOLUMES','AVG_LENGTH','AVG_SPACING','OCCUPANCY'])
-df = df.groupby(['EQUIPMENTID', 'LANE_BUNDLE_DIRECTION']).apply(lambda x: x.resample('H', on='AGG_PERIOD_START')["TOTAL_VOLUME"].sum())
+    'AGGREGATE_BY_LANE_BUNDLEID', 'AGG_ID', 'AGG_PERIOD_LEN_MINS', 'NR_LANES',
+    'LIGHT_VEHICLE_RATE', 'VOLUME_CLASSE_A', 'VOLUME_CLASSE_B', 'VOLUME_CLASSE_C',
+    'VOLUME_CLASSE_D', 'VOLUME_CLASSE_0', 'AVG_SPEED_ARITHMETIC', 'AVG_SPEED_HARMONIC',
+    'AXLE_CLASS_VOLUMES', 'AVG_LENGTH', 'AVG_SPACING', 'OCCUPANCY'])
+df = df.groupby(['EQUIPMENTID', 'LANE_BUNDLE_DIRECTION']).apply(
+    lambda x: x.resample('H', on='AGG_PERIOD_START')["TOTAL_VOLUME"].sum())
 print(df)
 df.to_csv("data/aggregated/2015-01-01.csv")
