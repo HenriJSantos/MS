05e4577bd71e7fc15d88fc990afef2cea6e96a0c
Helena Ferreira <ferreiraleninha52@gmail.com>
2022-01-17 12:34:42 +0000

    add image of origin-destination

--- code/weekday_aggregator.py
+++ code/weekday_aggregator.py
@@ -2,6 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("data/aggregated/agg.csv", header=0, sep=",", low_memory=False)
 df['AGG_PERIOD_START'] = pd.to_datetime(df['AGG_PERIOD_START'])
+
+#remove holidays
+df = df[(df['AGG_PERIOD_START'] != pd.to_datetime("2015-01-01")) &      # Dia de Ano Novo
+        (df['AGG_PERIOD_START'] != pd.to_datetime("2015-04-03")) &      # Sexta-Feira Santa
+        (df['AGG_PERIOD_START'] != pd.to_datetime("2015-05-01"))]       # Dia do Trabalhador
+
 df = df[df['AGG_PERIOD_START'].dt.dayofweek < 5]
 days = {0: 'Mon', 1: 'Tue', 2: "Wed", 3: "Thu", 4: "Fri"}
 df['WEEK_DAY'] = df['AGG_PERIOD_START'].apply(lambda x: days[x.dayofweek])
