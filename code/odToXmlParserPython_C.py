29bb761f2942a2c7ad7d5bed27ef236d76ac0a3c
nrm <gustavonrm@gmail.com>
2022-01-23 16:40:42 +0000

    new matrix wit c d

---
+++ code/odToXmlParserPython_C.py
@@ -0,0 +1,47 @@
+from fileinput import filename
+
+name = "matrizOD_c"
+fileNameRead = "../code/" + name + ".csv"
+fileNameWrite = "../Sumo/Simulation/od-matrixes-xml/" + name + ".xml"
+
+idName = "sim"
+startTime = "0.00"
+endTime = "3600.00"
+
+fr = open(fileNameRead, "r")
+fw = open(fileNameWrite, "w")
+
+textToWrite = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<data xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://sumo.dlr.de/xsd/datamode_file.xsd\">\n"
+textToWrite = textToWrite + \
+    "    <interval id=\"" + idName + "\" begin=\"" + \
+    startTime + "\" end=\"" + endTime + "\">\n"
+
+origin = 1
+destination = 1
+
+for line in fr:
+
+    values = line.split(',')
+    values[len(values) -
+           1] = values[len(values) - 1].replace('\n', '')
+
+    print(values)
+    # reset destination
+    destination = 1
+    for v in values:
+        textToWrite = textToWrite + "        <tazRelation from=\"EO" + \
+            str(origin) + "\" to=\"ED" + \
+            str(destination) + "\" count=\"" + \
+            str(v).replace('\n', '') + "\"/>\n"
+
+    origin = origin + 1
+
+    # ignore origin 11 since it is not included on the network
+    if origin == 10:
+        origin = origin + 1
+
+textToWrite = textToWrite + "    </interval>\n</data>"
+fw.write(textToWrite)
+
+fr.close()
+fw.close()
